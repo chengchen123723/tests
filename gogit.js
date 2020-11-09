2744165387584ca5ae53af23cf67ae537ea2fcbd
chengchen123723 <1760201748@qq.com>
2020-11-09 19:01:48 +0800

    'devs3'

--- gogit.js
+++ gogit.js
@@ -5,5 +5,8 @@ git commit -m '保存标识'： 提交代码
 git push origin 远程分支：往远程分支提交代码
 git log 查看修改日志  按q退出
 git checkout 分支名 进入某个分支
-git checkout -b 新分支名 origin/远程分支名： 基于远程分支创建新的分支1
+git checkout -b 新分支名 origin/远程分支名： 基于远程分支创建新的分支
+git branch -v: 查看本地分支
+git branch -a: 查看远程分支
+
 
